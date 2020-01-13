54ccb3b379d458182cb88c16893b90d69414dfbb
jpskgc <jpskgc.v3@gmail.com>
2020-01-13 19:58:23 +0900

    add base codes for local settings

--- Makefile
+++ Makefile
@@ -4,11 +4,15 @@ deps:
 	# go get -u ./...
 	go get -u github.com/aws/aws-lambda-go/events
 	go get -u github.com/aws/aws-lambda-go/lambda
+	go get -u github.com/aws/aws-lambda-go/events
+	go get -u github.com/aws/aws-lambda-go/lambda
+	go get -u github.com/aws/aws-sdk-go/aws
+	go get -u github.com/aws/aws-sdk-go/aws/session
+	go get -u github.com/aws/aws-sdk-go/service/dynamodb
+	go get -u github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute
 
 clean:
-	#rm -rf ./hello-world/hello-world
 	rm -rf ./positive-line-bot/positive-line-bot
 
 build:
-	#GOOS=linux GOARCH=amd64 go build -o hello-world/hello-world ./hello-world
 	GOOS=linux GOARCH=amd64 go build -o positive-line-bot/positive-line-bot ./positive-line-bot
--- positive-line-bot/main.go
+++ positive-line-bot/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	//"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var (
@@ -21,7 +31,117 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
+func exitWithError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
+
+type Item struct {
+	Key  int
+	Desc string
+	Data map[string]interface{}
+}
+
+func UnmarshalLineRequest(data []byte) (LineRequest, error) {
+	var r LineRequest
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *LineRequest) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type LineRequest struct {
+	Events      []Event `json:"events"`
+	Destination string  `json:"destination"`
+}
+
+type Event struct {
+	Type       string  `json:"type"`
+	ReplyToken string  `json:"replyToken"`
+	Source     Source  `json:"source"`
+	Timestamp  int64   `json:"timestamp"`
+	Message    Message `json:"message"`
+}
+
+type Message struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+
+type Source struct {
+	UserID string `json:"userId"`
+	Type   string `json:"type"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// myLineRequest, err := UnmarshalLineRequest([]byte(request.Body))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// bot, err := linebot.New(
+	// 	os.Getenv("CHANNELSECRET"),
+	// 	os.Getenv("ACCESSTOKEN"),
+	// )
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	endpoint := os.Getenv("DYNAMODB_ENDPOINT")
+	tableName := os.Getenv("DYNAMODB_TABLE_NAME")
+
+	sess := session.Must(session.NewSession())
+
+	config := aws.NewConfig().WithRegion("ap-northeast-1")
+	if len(endpoint) > 0 {
+		config = config.WithEndpoint(endpoint)
+	}
+
+	svc := dynamodb.New(sess, config)
+
+	params := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	result, err := svc.Scan(params)
+	if err != nil {
+		exitWithError(fmt.Errorf("failed to make Query API call, %v", err))
+	}
+
+	items := []Item{}
+
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &items)
+	if err != nil {
+		exitWithError(fmt.Errorf("failed to unmarshal Query result items, %v", err))
+	}
+
+	var words []string
+	for i, item := range items {
+		fmt.Printf("%d: Key: %d, Desc: %s\n", i, item.Key, item.Desc)
+		fmt.Printf("\tNum Data Values: %d\n", len(item.Data))
+		for k, v := range item.Data {
+			fmt.Printf("\t- %q: %v\n", k, v)
+			words = append(words, v.(string))
+		}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(words))
+	word := words[i]
+
+	log.Print(word)
+
+	// var tmpReplyMessage string
+	// tmpReplyMessage = word
+	// if _, err = bot.ReplyMessage(myLineRequest.Events[0].ReplyToken, linebot.NewTextMessage(tmpReplyMessage)).Do(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	resp, err := http.Get(DefaultHTTPGetAddress)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
@@ -39,6 +159,11 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	if len(ip) == 0 {
 		return events.APIGatewayProxyResponse{}, ErrNoIP
 	}
+
+	// return events.APIGatewayProxyResponse{
+	// 	Body:       "ok",
+	// 	StatusCode: 200,
+	// }, nil
 
 	return events.APIGatewayProxyResponse{
 		Body:       fmt.Sprintf("Hello, %v", string(ip)),
